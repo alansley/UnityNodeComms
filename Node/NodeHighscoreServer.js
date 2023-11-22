7c861f7425f943740f98ec9c1fcbdad2345420a0
alansley <aclansley@gmail.com>
2023-11-23 08:41:01 +1100

    Initial function test

---
+++ Node/NodeHighscoreServer.js
@@ -0,0 +1,25 @@
+// Import the express framework
+const express = require('express')
+
+// Create an instance of it
+const expressApp = express()
+
+// Set the port the server will listen on
+const port = 3000
+
+// When we receive a request at `http://localhost:3000/` respond with this result
+expressApp.get('/', (request, result) => {
+    SayHi(request, result);
+})
+
+// When we receive a request at `http://localhost:3000/` call the SayHi method
+//expressApp.get('/', SayHi(request, result));
+
+
+function SayHi(request, result)
+{
+	result.send('Hello from a function!');
+}
+
+
+expressApp.listen(port, () => console.log('Node server started and listening on port: ' + port));
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UnityNodeComms
-A Unity and Node.js (simulated) geographic online high-score example project
+A Unity and Node.js (simulated) geographic online high-score example project.
+
+## Currently WIP - I just felt like learning some Node.js =D
